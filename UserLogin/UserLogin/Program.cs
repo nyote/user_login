f6c0eaae00d2c212b957daa8b2caa24778ea57f7
nyote <nyote@users.noreply.github.com>
2021-01-25 21:00:26 -0600

    Little user login creation in C#

---
+++ UserLogin/UserLogin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UserLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isUser = false;
+            Console.WriteLine("Registration...");
+            Console.WriteLine("Please enter a username: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Please enter the password: ");
+            string passwd = Console.ReadLine();
+            Console.WriteLine("Now that the registration is finished, you may enter the credentials");
+
+            Console.WriteLine("Username:");
+            string newUname = Console.ReadLine();
+            Console.WriteLine("Password:");
+            string newpasswd = Console.ReadLine();
+            if(userName == newUname && passwd == newpasswd)
+            {
+                isUser = true;
+            }
+            else
+            {
+                isUser = false;
+            }
+            Console.WriteLine(isUser);
+        }
+    }
+}
